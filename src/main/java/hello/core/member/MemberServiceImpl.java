818caa71c437e3a4e9957b3d2fec24f086797e36
myungkui <audrb96@gmail.com>
2021-12-22 23:42:12 +0900

    Singleton

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -19,4 +19,8 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId){
         return memberRepository.findById(memberId);
     }
+    //테스트용
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
